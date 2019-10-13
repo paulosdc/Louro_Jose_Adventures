899a53341bc3bc2984c1156849a852a72830aaa3
Lindenjefferson <lindenjeffersom@hotmail.com>
2019-10-12 21:26:37 -0300

    Parte 2

--- sketch.js
+++ sketch.js
@@ -1,38 +1,54 @@
-  var x = 50;
-    var y = 280;
-    var dir = 0;
-    var m = 0;
-    function setup() {
-      createCanvas(400, 400);
-    }
-    function keyPressed() {
-      if (keyCode === UP_ARROW) {
-      dir = 1;
+var x = 0, y = 150, up = false, altM, vivo = true;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    up = true;
+    altM = y - 40;
+  }
+}
+
+function cenario() {
+  x += 3;
+  translate(-x, 0);
+  background(220);
+  var altura = random(50,250);
+  var distancia = 175;
+  for(var a = 1; a<=250; a++){
+    rect(300 + distancia*a, altura+90, 30, 300);
+    rect(300 + distancia*a, -1, 30, altura);
+  }
+}
+
+function movimentacao() {
+  if (up == true)  {
+    y-=4;
+    if (y == altM) {
+      up = false;
     }
   }
-  function draw() {
-    background("#00FF7F");
-    square(0,300,400);//chão
-    rect(350, 250, 20, 50);//obstáculo
-    if (keyIsDown(LEFT_ARROW)) {//ir pra esquerda
-      x-=5;
+  if (up == false)  {
+    if (y  != 380) {
+      y+=2;
     }
-    if (keyIsDown(RIGHT_ARROW))  {//ir pra direita
-      x+=5;
-    }
-    if (dir==1)  {//subir e, ao atingir uma altura máxima, descer
-      for(var i = 0; i<=1; i++) {
-        if(y!=230){
-          y-=1;
-          if(y==231)
-          m = 1;
-        }
-      }
-      if (dir==1){//descida
-        if(m==1 && y<280){
-          y+=3;
-        }
-      }
-    }
-    ellipse(x,y,40,40);
   }
+}
+
+function estado() {
+   if (y == 20 || y ==380) {
+      vivo = false;
+   }
+  return vivo;
+}
+
+function draw() {
+  cenario();
+  movimentacao();
+  if (estado() == false) {
+    
+  }
+  ellipse(x+60, y,40,40);
+}
